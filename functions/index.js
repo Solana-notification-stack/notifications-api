451aa21c9cab8a9013367b03f16415a4e61a7fab
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-03-27 13:13:32 +0530

    add organisation endpoints

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,21 @@ const cors = require("cors")
 const app = express()
 const {setGlobalOptions} = require("firebase-functions/v2");
 const {onDocumentCreated} = require("firebase-functions/v2/firestore");
-const nodemailer = require("nodemailer")
 
 require("dotenv").config()
 
 setGlobalOptions({maxInstances: 10});
 app.use(cors());
 const {onRequest} = require("firebase-functions/v2/https");
-const { db } = require("./util/admin");
+const { db } = require("./utils/admin");
+const { signUpOrganisation, loginOrganisation, getAuthenticatedOrganisation, forgotPassword } = require("./handlers/Organisations");
+const FBAuth = require("./utils/FBAuth")
+
+
+//users
+app.post("/signUpOrganisation",signUpOrganisation)
+app.post("/loginOrganisation",loginOrganisation)
+app.post("/resetPassword",forgotPassword)
+app.get("/organisationData",FBAuth,getAuthenticatedOrganisation)
+
+exports.api =  onRequest(app)
--- functions/utils/Validators.js
+++ functions/utils/Validators.js
@@ -11,25 +11,20 @@ const isEmail = (email) => {
   
   exports.validateSignupData = (data) => {
     let errors = {};
-  
     if (isEmpty(data.email)) {
       errors.email = 'Must not be empty';
     } else if (!isEmail(data.email)) {
       errors.email = 'Must be a valid email address';
     }
-
-    
     if (isEmpty(data.name)) errors.name = 'Must not be empty';
-    if (isEmpty(data.confirmPassword)) errors.confirmPassword = 'Must not be empty';
-    if (isEmpty(data.phone)) errors.phone= 'Must not be empty';
-    if (isEmpty(data.college)) errors.college = 'Must not be empty';
-
-  
+    if (isEmpty(data.confirmPassword)) errors.confirmPassword = 'Must not be empty'
     if (isEmpty(data.password)) errors.password = 'Must not be empty';
     if (data.password !== data.confirmPassword)
       errors.confirmPassword = 'Passwords must match';
     // if (isEmpty(data.handle)) errors.handle = 'Must not be empty';
-  
+  if(data.name.length < 5 ){
+    errors.name = "Name sholud be atleast 5 letters"
+  }
     return {
       errors,
       valid: Object.keys(errors).length === 0 ? true : false
